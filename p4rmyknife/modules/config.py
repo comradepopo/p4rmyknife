b19f05e9cf7eb6532429437818aa3f7864424cfd
Robert <robert.warmack@gmail.com>
2019-10-18 09:24:59 -0700

    mmaking it a bit more clear

--- p4rmyknife/modules/config.py
+++ p4rmyknife/modules/config.py
@@ -1,6 +1,15 @@
 import urllib.request
 
 def run(args):
+    if args.install_client:
+      install_client(args)
+
+
+
+def install_client(args):
+    # Set up the p4 client environment.
+
+    # First, download the platform-specific binary.
     if args.os.startswith('linux'):
       p4_url = 'http://www.perforce.com/downloads/perforce/r19.2/bin.linux26x86_64/p4v.tgz'
       urllib.request.urlretrieve(p4_url, 'p4v.tgz')
