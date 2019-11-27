e5b3d0a2755609172a4f464c3eceaef84edadade
Louis Khalifeh <louis@assembla.com>
2019-11-27 12:38:30 -0600

    config.py addition+fix 1
    Moved urllib lines outside of the `if` statement.
    Added `try/except` block to verify that the downloaded file exists in working directory.
    Added notes on what next goals are.

--- p4rmyknife/modules/config.py
+++ p4rmyknife/modules/config.py
@@ -11,8 +11,18 @@ def install_client(args):
 
     # First, download the platform-specific binary.
     if args.os.startswith('linux'):
-      #p4_url = 'http://www.perforce.com/downloads/perforce/r19.2/bin.linux26x86_64/p4v.tgz'
-      #The below line (line 16) is for testing purposes only, and is not intended for the final product.
-       p4_url = 'https://louis-test-p4k.s3.us-east-2.amazonaws.com/TestFile.sh'
-      #urllib.request.urlretrieve(p4_url, 'p4v.tgz')
-       urllib.request.urlretrieve(p4_url, 'TestFile.sh')
+        #p4_url = 'http://www.perforce.com/downloads/perforce/r19.2/bin.linux26x86_64/p4v.tgz'
+        #The below line (line 16) is for testing purposes only, and is not intended for the final product.
+        p4_url = 'https://louis-test-p4k.s3.us-east-2.amazonaws.com/TestFile.sh'
+    #urllib.request.urlretrieve(p4_url, 'p4v.tgz')
+    urllib.request.urlretrieve(p4_url, 'TestFile.sh')
+    #Verify file exists below.
+    try:
+        f =open("TestFile.sh")
+        f.close()
+    except:
+        print ("File is not accessible.")
+    
+    
+    #11/27/19 Need to add lines that will output 200s or the standard range of error codes (300,400,500s) if there is a server-side issue.
+    #11/27/19 cont.- Code to output any client-side errors should also be added. Next to add code that will execute/run the downloaded file.
